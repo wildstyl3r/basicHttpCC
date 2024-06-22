b1cbcaaa655f5ac1a66bcde08797f6969d469894
Mike <wildstyl3r@yandex.ru>
2024-06-22 22:39:00 +0300

    add cyclic response and url parsing

--- app/server.go
+++ app/server.go
@@ -2,10 +2,44 @@ package main
 
 import (
 	"fmt"
+	"net/url"
+
 	// Uncomment this block to pass the first stage
 	"net"
 	"os"
+	"strings"
 )
+
+type Request struct {
+	Method  string
+	Url     *url.URL
+	Headers []string
+	Body    string
+}
+
+func readRequest(b []byte) (Request, error) {
+	r := string(b[:])
+	bodyStart := strings.Index(r, "\r\n\r\n")
+	if bodyStart == -1 {
+		bodyStart = len(r) - 1
+	}
+	head := strings.Split(r[:bodyStart], "\r\n")
+	requestLine := strings.Split(head[0], " ")
+	method := requestLine[0]
+	url, err := url.Parse(requestLine[1])
+	if err != nil {
+		return Request{}, err
+	}
+
+	body := r[bodyStart+len("\r\n\r\n"):]
+
+	return Request{
+		Method:  method,
+		Url:     url,
+		Headers: head[1:],
+		Body:    body,
+	}, nil
+}
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -19,10 +53,35 @@ func main() {
 		os.Exit(1)
 	}
 
-	conn, err := l.Accept()
-	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		buffer := make([]byte, 4096)
+		_, err = conn.Read(buffer)
+
+		if err != nil {
+			fmt.Println("Error reading from connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		req, err := readRequest(buffer)
+
+		if err != nil {
+			fmt.Println("Error parsing request: ", err.Error())
+			os.Exit(1)
+		}
+
+		fmt.Println(req.Url.Path)
+
+		if req.Url.Path == "/" {
+			conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+		} else {
+			conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+		}
 	}
 }
