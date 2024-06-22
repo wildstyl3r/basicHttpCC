a97534c9cc1deb8fa027ee5040daf6a36178f2e1
Mike <wildstyl3r@yandex.ru>
2024-06-22 20:51:03 +0300

    write response to tcp connection

--- app/server.go
+++ app/server.go
@@ -19,7 +19,8 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	conn, err := l.Accept()
+	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
